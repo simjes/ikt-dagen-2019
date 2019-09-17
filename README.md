99923b1aa73b86b5d08c1d006c3a9401c74f289a
simjes <simjes91@gmail.com>
2019-09-17 16:51:59 +0200

    Add solution

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Dfind - IKT-dagen 2019
-
-Solution will be published when the competition is done.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // a and b are numbers
 // e.g. 5 + 5 = 10
 function sum(a, b) {
-  return a + a + b;
+  return a + b;
 }
 
 // find the sum of every odd number from 0 up to and including `number`
@@ -10,7 +10,9 @@ function findSumOfOddNumbers(number) {
   let sum = 0;
 
   for (let index = 0; index <= number; index++) {
-    sum += index;
+    if (index % 2 !== 0) {
+      sum += index;
+    }
   }
 
   return sum;
@@ -19,13 +21,13 @@ function findSumOfOddNumbers(number) {
 // `numbers` is an array that only contains numbers
 // e.g. [1 , 5 , 2 , 9, 32]
 function reverseNumbers(numbers) {
-  return numbers;
+  return numbers.reverse();
 }
 
 // `numbers` is an array that only contains numbers
 // e.g. [1 , 5 , 2 , 9, 32]
 function doubleNumbersInList(numbers) {
-  return numbers.map();
+  return numbers.map(number => number * 2);
 }
 
 // `people` is a list of people. A person has the following structure:
@@ -39,35 +41,40 @@ function doubleNumbersInList(numbers) {
 //     "address": "745 Forest Place, Frank, Oregon, 3078"
 //  }
 function findNumberOfFemales(people) {
-  return people.length;
+  return people.filter(person => person.gender === 'female').length;
 }
 
 // `people` is a list of people.
 function findEmailsOfMalesOver30(people) {
-  return people.filter(person => person.age < 30).map(person => person.email);
+  return people
+    .filter(person => person.gender === 'male' && person.age > 30)
+    .map(person => person.email);
 }
 
 // `people` is a list of people.
 function sumOfAllAges(people) {
-  throw new Error('not implemented');
+  return people.reduce((sum, person) => {
+    return sum + person.age;
+  }, 0);
 }
 
 // `numbers` is an array that only contains numbers
 // e.g. [1 , 5 , 5, 2 , 9, 5, 32]
 // [...new Set(numbers)] is a copy of `numbers`, and without duplicates
 function removeDuplicatesAndSortNumbers(numbers) {
-  return [...new Set(numbers)].sort((a, b) => '...');
+  return [...new Set(numbers)].sort((a, b) => a - b);
 }
 
 // find the product of all positive numbers less than and equal to `number`
 // e.g. if number = 3, then the answer is the sum of 1*2*3
 function firstFactorial(number) {
-  throw new Error('not implemented');
+  if (number === 1) return 1;
+  return number * firstFactorial(number - 1);
 }
 
 // startDate and endDate are date objects
 function secondsBetweenDates(startDate, endDate) {
-  return endDate.getTime() / 1000;
+  return (endDate.getTime() - startDate.getTime()) / 1000;
 }
 
 module.exports = {
